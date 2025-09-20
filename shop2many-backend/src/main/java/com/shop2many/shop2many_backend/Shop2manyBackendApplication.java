8cab11e39dccd93d45e3b6498bfaef4ee671e96c
Akash Singh <akashsinghaa008@gmail.com>
2025-09-20 10:28:05 +0530

    Add initial project structure and configuration files for Shop2Many backend

---
+++ shop2many-backend/src/main/java/com/shop2many/shop2many_backend/Shop2manyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shop2many.shop2many_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop2manyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop2manyBackendApplication.class, args);
+	}
+
+}
---
+++ shop2many-backend/src/test/java/com/shop2many/shop2many_backend/Shop2manyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop2many.shop2many_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop2manyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
